10bd53d76a33e0468861f080fd363541631cade3
GjemaVisar <visar.gjema@student.uni-pr.edu>
2025-09-20 17:52:56 +0200

    feat(i18n): add language switcher with SQ default and EN option

--- medi_guide/src/app/src/components/landing_page/landing-page.component.ts
+++ medi_guide/src/app/src/components/landing_page/landing-page.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
-
+  imports: [CommonModule, TranslateModule],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
-  constructor(private router: Router) {}
+  currentLang = 'sq';
+
+  constructor(private router: Router, private translate: TranslateService) {
+    this.translate.addLangs(['en', 'sq']);
+    this.translate.setDefaultLang('sq');
+    this.translate.use('sq');
+  }
 
   startAssessment() {
     this.router.navigate(['/assessment']);
+  }
+
+  switchLang(lang: string) {
+    this.currentLang = lang;
+    this.translate.use(lang);
   }
 }
